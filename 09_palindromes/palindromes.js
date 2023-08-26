15ccf69c422181af8f702d84c713a54963a3706a
8maaan <johnrandolf696969@gmail.com>
2023-08-26 21:54:42 +0800

    Answered.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    const cleanString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+    let left = 0;
+    let right = cleanString.length - 1;
+
+    while (left < right) {
+        if (cleanString[left] !== cleanString[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
 };
+
+
+// console.log(palindromes("racecar"));
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let index = parseInt(n);
+
+    if(index < 0){
+        return "OOPS";
+    }
+
+    if(index === 1 || index === 2){
+        return 1;
+
+    }else if(index === 0){
+        return 0;
+    }
+
+    let firstPrevious = 1;
+    let secondPrevious = 0;
+    let currentIndex = 0;
+    for(let i = 2; i <= index; i++){
+        currentIndex = firstPrevious + secondPrevious;
+        secondPrevious = firstPrevious;
+        firstPrevious = currentIndex;
+    }
+
+    return currentIndex;
 
 };
+
+//console.log(fibonacci(5));
 
 // Do not edit below this line
 module.exports = fibonacci;
